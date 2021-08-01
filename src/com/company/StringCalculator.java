90a2eb14696a863b6307d21f606f2925db833268
whoamixzerone <whoamixzerone@gmail.com>
2021-08-01 21:26:14 +0900

    첫번째 리팩토

--- src/com/company/StringCalculator.java
+++ src/com/company/StringCalculator.java
@@ -7,16 +7,31 @@ public class StringCalculator {
             return 0;
         }
 
-        if(text.contains(",")) {
-            String[] values = text.split(",");
-            int sum = 0;
-            for (String value : values) {
-                sum += Integer.parseInt(value);
-            }
-            return sum;
+        String[] values = text.split(",");
+        int sum = 0;
+        for (String value : values) {
+            sum += Integer.parseInt(value);
         }
-
-        return Integer.parseInt(text);
+        return sum;
     }
 
+    // 1단계 리팩토링
+    public int add_refact1(String text) {
+        if(text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        // 숫자 하나일 때 "," 없이 split 해도 숫자 하나를 반환하므로
+        // if, 숫자형으로 형변환하고 반환하는게 필요없음
+        //if(text.contains(",")) {
+        String[] values = text.split(",");
+        int sum = 0;
+        for (String value : values) {
+            sum += Integer.parseInt(value);
+        }
+        return sum;
+        //}
+
+        //return Integer.parseInt(text);
+    }
 }
---
+++ test/com/company/SplitTest.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SplitTest {
+
+    @Test
+    public void split() {
+        String[] values = "1".split(",");
+        assertArrayEquals(new String[] {"1"}, values);
+
+        values = "1,2".split(",");
+        assertArrayEquals(new String[] {"1", "2"}, values);
+
+    }
+}
